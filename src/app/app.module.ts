96f98027f3dd15789708dc84a5035da36bf8dfda
Jorge Perez Tirado <jptirado@minsait.com>
2023-07-25 10:32:02 +0200

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.modules';
+
 
 import { AppComponent } from './app.component';
+// import { HeroComponent } from './heroes/hero/hero.component';
+// import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
